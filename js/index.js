90a0ec05f829bd25023286d907cdd1759841c640
Aamir <aamirhafiez@gmail.com>
2020-09-26 13:04:11 +0530

    created a todo list web app

---
+++ js/index.js
@@ -0,0 +1,76 @@
+var numberOfTasks = 0;
+var taskNumber = 1; 
+var inc = 'true';
+
+// checking number of tasks present
+let noTask = function(){
+    if(numberOfTasks == 0){
+        document.getElementById('tasks-container').innerHTML = `<h6 style="color: #1badf7">Go on and add some tasks!!</h6>`;
+    }else if(numberOfTasks == 1 && inc == 'true'){
+        document.getElementById('tasks-container').innerHTML = `<p id="append"></p>`;
+        inc = 'false';
+    }else if(numberOfTasks == 1 && inc == 'false'){
+        inc = 'true';
+    }
+}
+noTask();
+
+// total tasks present
+let tasksPresent = function(){
+    document.getElementById('tasks-left').innerHTML = numberOfTasks;
+
+}
+
+// Add a task on keypress enter
+let userInput = document.getElementById('text-box');
+userInput.addEventListener('keypress', function(e){  
+    let text;
+    if(e.key === "Enter"){
+        // getting text from textbox
+        text = userInput.value;
+        if(text === ""){
+            return;
+        }
+        // increase number of tasks
+        numberOfTasks += 1;
+        // checks if no task is left then shows a statement to add tasks
+        noTask();
+        // text is cleared from textbox
+        userInput.value = "";
+        // crating a new task with required values
+        let addTask = `<div id="task${taskNumber}" class="task p-2 d-flex justify-content-between">
+        <div>
+            <input class="check-task" type="checkbox" title="Check if completed!">
+            <span>${text}</span>
+        </div>
+        <div class="delete-icon" style="cursor: pointer;">
+            <i id="${taskNumber}" class="delete far fa-times-circle" title="Delete this task!"></i>
+        </div>
+        </div>`;
+
+        // inserting the new task before p tag in the task-container
+        document.getElementById('append').insertAdjacentHTML("beforebegin", addTask);
+        
+        // increasing number of tasks
+        taskNumber += 1;
+        // changes the UI of total tasks in the list
+        tasksPresent();
+    }
+});
+
+// adding event listener to whole document (event delegation)
+document.addEventListener('click',function(e){
+    // deleting a task
+    if(e.target && e.target.className === 'delete far fa-times-circle'){
+        // getting task id
+        let taskId = e.target.id;
+        // removing the html of that task
+        document.getElementById(`task${taskId}`).remove();
+        // decreasing total tasks
+        numberOfTasks--;
+        // checks if no task is left then shows a statement to add tasks
+        noTask();
+        // changes the UI of total tasks in the list
+        tasksPresent();
+    }
+ });
